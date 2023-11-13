19ecb501d81e17e192a7dd66681eae89f26170e2
Anton <anton.batalov94@gmail.com>
2023-11-13 19:14:00 +0700

    task 13.4.4

--- Module13/Program.cs
+++ Module13/Program.cs
@@ -5,24 +5,59 @@ namespace Module13
 {
     internal class Program
     {
-        //task 13.3.10
+        //task 13.4.4
         static void Main(string[] args)
         {
-            string text = "Подсчитайте, сколько уникальных символов в этом предложении, используя HashSet<T>, учитывая знаки препинания, но не учитывая пробелы в начале и в конце предложения.";
 
-            var set  = new HashSet<char>();
+            var contacts = new Dictionary<string, Contact>();
 
-            foreach (char c in text)
+            contacts.Add("Игорь", new Contact("Игорь", 79990000000, "igor@example.com"));
+            contacts.Add("Андрей", new Contact("Андрей", 79990000001, "andrew@example.com"));
+
+            foreach (var contact in contacts)
             {
-                    set.Add(c);
-              
+                Console.WriteLine($"{contact.Key} : {contact.Value.PhoneNumber}");
+            }
+            Console.WriteLine();
+
+            var anton = new Contact("Антон", 79990000002, "anton@example.com");
+            contacts.Add(anton.Name, anton);
+
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine($"{contact.Key} : {contact.Value.PhoneNumber}");
             }
 
-            Console.WriteLine(set.Count);
+            Console.WriteLine();
+
+
+            if (contacts.ContainsKey("Антон"))
+            {
+                contacts["Антон"].PhoneNumber = 333444;
+            }
+
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine($"{contact.Key} : {contact.Value.PhoneNumber}");
+            }
         }
 
 
 
+    }
+
+    public class Contact // модель класса
+    {
+        public Contact(string name, long phoneNumber, String email) // метод-конструктор
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public String Name { get; }
+        public long PhoneNumber { get; set; }
+        public String Email { get; }
     }
 
 }
