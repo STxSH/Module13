cb6833665ee0a4e533fe02ef39fc4e45ee3074e9
STxSH <anton.batalov94@gmail.com>
2023-11-15 13:59:35 +0700

    final tasks done

---
+++ FinalTask1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+
+namespace FinalTask1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText("D:\\YandexDisk\\Синхронизация\\Skillfactory\\Module13\\FinalTask1\\Text1.txt");
+
+            char[] delimiters = { ' ', '\r', '\n', ',', '.', ';', ':', '!', '?' };
+            var words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> wordList = new List<string>();
+            LinkedList<string> wordLinkedList = new LinkedList<string>();
+
+            var watchOne = Stopwatch.StartNew();
+            foreach (var word in words)
+            {
+                wordList.Add(word);
+            }
+            watchOne.Stop();
+            Console.WriteLine($"Вставка в List: {watchOne.Elapsed.TotalMilliseconds} мс");
+            //Вставка в List: 2,6105 мс
+
+            var watchTwo = Stopwatch.StartNew();
+            foreach (var word in words)
+            {
+                wordLinkedList.AddLast(word);
+            }
+            watchTwo.Stop();
+            Console.WriteLine($"Вставка в LinkedList: {watchTwo.Elapsed.TotalMilliseconds} мс");
+            //Вставка в LinkedList: 18,5717 мс
+
+
+        }
+    }
+}
---
+++ FinalTask2/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace FinalTask2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText("D:\\YandexDisk\\Синхронизация\\Skillfactory\\Module13\\FinalTask2\\Text1.txt");
+
+            string noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+            char[] delimiters = { ' ', '\r', '\n', };
+            var words = noPunctuationText.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                if (wordCount.ContainsKey(word))
+                {
+                    wordCount[word]++;
+                }
+                else
+                {
+                    wordCount[word] = 1;
+                }
+            }
+
+            var mostCommonWords = wordCount.OrderByDescending(c => c.Value);
+
+            Console.WriteLine("Самые часто встречающиеся слова:");
+            int displayedWords = 20;
+
+            foreach (var pair in mostCommonWords)
+            {
+                Console.WriteLine($"{pair.Key}: {pair.Value} раз");
+                displayedWords--;
+                if (displayedWords == 0) break;
+            }
+            Console.ReadLine();
+            /*
+                Самые часто встречающиеся слова:
+                и: 5716 раз
+                не: 3644 раз
+                в: 3337 раз
+                на: 2303 раз
+                он: 1989 раз
+                с: 1977 раз
+                что: 1948 раз
+                как: 1304 раз
+                она: 1244 раз
+                я: 1044 раз
+                а: 1002 раз
+                Обломов: 911 раз
+                его: 886 раз
+                все: 869 раз
+                к: 782 раз
+                у: 762 раз
+                это: 735 раз
+                так: 715 раз
+                за: 671 раз
+                Он: 636 раз
+            */
+        }
+    }
+}
